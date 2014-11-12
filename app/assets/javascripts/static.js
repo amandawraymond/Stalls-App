f3874e53cce033ecf00123c982b507fd976a8b08
amandawraymond <amandawraymond@gmail.com>
2014-11-12 13:57:46 -0500

    git initialize

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,55 @@
+angular.module('leatherLaneMarketApp', ['ngAnimate'])
+     .controller('MarketController', ["$scope", function($scope){
+       $scope.stalls = stalls;
+
+       $scope.selectStall= function(stall) {
+        $scope.selectedStall = stall;
+       };
+
+       $scope.clearSelectedStall = function() {
+        $scope.selectedStall = false;
+       };
+
+       $scope.addStall = function() {
+        $scope.stalls.push($scope.newStall);
+        $scope.newStall = false;
+        $scope.stallForm.$setPristine();
+       };
+
+
+     }]);
+
+stalls = [
+  {
+    name: "Burrito",
+    price: 5,
+    description:  "Meat and vegetables in a delicious wrap"
+  },
+  {
+    name: "Pizza",
+    price: 6.5,
+    description:  "Cheese and meat and veg on some dough"
+  },
+  {
+    name: "Fallafel",
+    price: 4.5,
+    description: "Deep fried delicious chickpeas"
+  },
+   {
+    name: "Club Sammy",
+    price: 5,
+    description:  "Meat and vegetables in a delicious wrap"
+  },
+  {
+    name: "Steak Melt",
+    price: 6.5,
+    description:  "Cheese and meat and veg on some dough"
+  },
+  {
+    name: "Chips",
+    price: 4.5,
+    description: "Deep fried delicious chickpeas"
+  }
+]
+
+  
