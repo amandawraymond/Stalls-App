f0bbf66b1dc89ffc2e4772d50e003e79c061f75f
amandawraymond <amandawraymond@gmail.com>
2014-11-13 11:02:22 -0500

    google maps showing on page

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -21,6 +21,7 @@ $(function(){
       }
     })
 
+
       function updateLocation(position){
         var coords = position.coords;
         var latlng = new google.maps.LatLng(coords.latitude,coords.longitude);
@@ -42,69 +43,69 @@ $(function(){
       var marker = new google.maps.Marker({
         map: map
       });
+     })
 
 
-      var input = document.getElementById('autocomplete')
-      var autocomplete = new google.maps.places.Autocomplete(input);
+      // var input = document.getElementById('autocomplete')
+      // var autocomplete = new google.maps.places.Autocomplete(input);
 
-      autocomplete.bindTo('bounds', map);
+      // autocomplete.bindTo('bounds', map);
 
-      google.maps.event.addListener(autocomplete, 'place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
+      // google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      //   infowindow.close();
+      //   marker.setVisible(false);
+      //   var place = autocomplete.getPlace();
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-          map.fitBounds(place.geometry.viewport);
-        } else {
-          map.setCenter(place.geometry.location);
-          map.setZoom(17)
-        }
+      //   // If the place has a geometry, then present it on a map.
+      //   if (place.geometry.viewport) {
+      //     map.fitBounds(place.geometry.viewport);
+      //   } else {
+      //     map.setCenter(place.geometry.location);
+      //     map.setZoom(17)
+      //   }
 
-        marker.setIcon({
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(35, 35)
-        });
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
+      //   marker.setIcon({
+      //     url: place.icon,
+      //     size: new google.maps.Size(71, 71),
+      //     origin: new google.maps.Point(0, 0),
+      //     anchor: new google.maps.Point(17, 34),
+      //     scaledSize: new google.maps.Size(35, 35)
+      //   });
+      //   marker.setPosition(place.geometry.location);
+      //   marker.setVisible(true);
 
-        var address = '';
-        if (place.address_components) {
-          address = (place.address_components[0] && place.address_components[0].short_name || '')
-        }
+      //   var address = '';
+      //   if (place.address_components) {
+      //     address = (place.address_components[0] && place.address_components[0].short_name || '')
+      //   }
 
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
-      });
+      //   infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      //   infowindow.open(map, marker);
+      // });
 
-    $("#directions_form").on("submit", function(){
-        from = $("#directions_from").val()
-        to = $("#directions_to").val()
-        mode = $("#directions_mode").val()
-        var request = {
-            origin:from,
-            destination:to,
-            travelMode: google.maps.TravelMode[mode]
-      };
+    // $("#directions_form").on("submit", function(){
+    //     from = $("#directions_from").val()
+    //     to = $("#directions_to").val()
+    //     mode = $("#directions_mode").val()
+    //     var request = {
+    //         origin:from,
+    //         destination:to,
+    //         travelMode: google.maps.TravelMode[mode]
+    //   };
 
-        directionsService = new google.maps.DirectionsService();
-        directionsDisplay = new google.maps.DirectionsRenderer();
-        directionsDisplay.setMap(map);
-        directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            $("#directions-panel").html()
-            directionsDisplay.setPanel(document.getElementById('directions-panel'));
+    //     directionsService = new google.maps.DirectionsService();
+    //     directionsDisplay = new google.maps.DirectionsRenderer();
+    //     directionsDisplay.setMap(map);
+    //     directionsService.route(request, function(response, status) {
+    //       if (status == google.maps.DirectionsStatus.OK) {
+    //         directionsDisplay.setDirections(response);
+    //         $("#directions-panel").html()
+    //         directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
-          }
-          else{
-            alert("something went wrong!")
-          }
-        });        
-        return false;
+    //       }
+    //       else{
+    //         alert("something went wrong!")
+    //       }
+    //     });        
+    //     return false;
 
-  })
